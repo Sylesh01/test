9b342098220f98ef74c36f5206dc427df66bdcb6
Sylesh01 <102213775+Sylesh01@users.noreply.github.com>
2023-05-24 15:20:41 +0530

    Add files via upload

---
+++ Pages.php
@@ -0,0 +1,12 @@
+<?php
+  class Pages {
+    public function __construct(){
+        echo 'Pages controller is working';
+    }
+    public function index(){
+      echo 'index method working';
+    }
+    public function about(){
+        echo 'This is the about method';
+    }
+  }
